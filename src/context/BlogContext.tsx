b00064ef1aed13badb48b948e0c336dee299d779
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-27 17:20:59 +0300

    rendered posts frm context

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -2,7 +2,16 @@ import React from "react";
 const BlogContext = React.createContext({});
 
 export const BlogProvider: React.FC = ({ children }) => {
-  return <BlogContext.Provider value={{}}>{children}</BlogContext.Provider>;
+  const blogPosts = [
+    { title: "Blog#1" },
+    { title: "Blog#2" },
+    { title: "Blog#3" },
+    { title: "Blog#4" },
+    { title: "Blog#5" }
+  ];
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const posts = useContext(BlogContext) as { title: string }[];
+
   return (
     <View>
       <Text>IndexScreen IndexScreen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={data => data.title}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
     </View>
   );
 };
