191b4577616827286c62fe680e5255e4dee9dbb3
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-28 13:52:55 +0300

    wired up BlogPostForm on edit

---
+++ src/components/BlogPostForm.tsx
@@ -0,0 +1,62 @@
+import React, { useContext, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+
+interface Props {
+  titleLabel: string;
+  contentLabel: string;
+  btnLabel: string;
+  title?: string;
+  content?: string;
+}
+
+const BlogPostForm: React.FC<Props> = props => {
+  const [title, setTitle] = useState<string>(props.title || "");
+  const [content, setContent] = useState<string>(props.content || "");
+  return (
+    <View>
+      <Text style={styles.text}>{props.titleLabel}:</Text>
+      <TextInput style={styles.input} onChangeText={setTitle} value={title} />
+      <Text style={styles.text}>{props.contentLabel}:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setContent}
+        value={content}
+      />
+      <TouchableOpacity style={styles.submit}>
+        <Text style={{ color: "white" }}>{props.btnLabel}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BlogPostForm;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: 20,
+    marginTop: 10
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    marginHorizontal: 20,
+    paddingTop: 15,
+    paddingBottom: 2,
+    marginTop: 10,
+    fontSize: 18
+  },
+  submit: {
+    backgroundColor: "#00a2ff",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    width: 350,
+    alignSelf: "center",
+    borderRadius: 5
+  }
+});
--- src/screens/EditScreen.tsx
+++ src/screens/EditScreen.tsx
@@ -4,58 +4,25 @@ import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import { Context } from "../context/BlogContext";
 import { BlogContext } from "./IndexScreen";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen: NavigationStackScreenComponent<{ id?: number }> = props => {
   const { state } = useContext(Context) as BlogContext;
   const blog = state.blogPosts.find(
     pb => pb.id === props.navigation.getParam("id")
   );
-  const [title, setTitle] = useState<string>(blog?.title || "");
-  const [content, setContent] = useState<string>(blog?.content || "");
+  const { title, content } = blog as typeof state.blogPosts[0];
   return (
     <View>
-      <Text style={styles.text}>Edit Title:</Text>
-      <TextInput style={styles.input} onChangeText={setTitle} value={title} />
-      <Text style={styles.text}>Edit Content:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setContent}
-        value={content}
+      <BlogPostForm
+        titleLabel="Edit Title"
+        contentLabel="Edit Content"
+        btnLabel="Edit Blog"
+        title={title}
+        content={content}
       />
-      <TouchableOpacity style={styles.submit}>
-        <Text style={{ color: "white" }}>Edit Blog</Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 export default EditScreen;
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginHorizontal: 20,
-    marginTop: 10
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "black",
-    marginHorizontal: 20,
-    paddingTop: 15,
-    paddingBottom: 2,
-    marginTop: 10,
-    fontSize: 18
-  },
-  submit: {
-    backgroundColor: "#00a2ff",
-    height: 50,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-    width: 350,
-    alignSelf: "center",
-    borderRadius: 5
-  }
-});
