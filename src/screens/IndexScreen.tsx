5624c781bec83787a4fc7d6608f0aa65cc4733a6
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-28 10:16:26 +0300

    wired up navigation to ShowScreen

--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import { Context } from "../context/BlogContext";
 import { MaterialIcons } from "@expo/vector-icons";
+import { StackNavigationProp } from "react-navigation-stack/lib/typescript/src/vendor/types";
 
 interface BlogContext {
   state: { blogPosts: { title: string }[] };
@@ -10,7 +11,7 @@ interface BlogContext {
   removeBlogPost: (title: string) => void;
 }
 
-const IndexScreen = () => {
+const IndexScreen: React.FC<{ navigation: StackNavigationProp }> = props => {
   const {
     state: { blogPosts },
     addBlogPosts,
@@ -34,14 +35,15 @@ const IndexScreen = () => {
         data={blogPosts}
         keyExtractor={data => data.title}
         renderItem={({ item }) => (
-          <View style={styles.post}>
+          <TouchableOpacity
+            style={styles.post}
+            onPress={() => props.navigation.navigate("Show")}
+          >
             <Text>{item.title}</Text>
-            <MaterialIcons
-              name="delete"
-              size={25}
-              onPress={() => removeBlogPost(item.title)}
-            />
-          </View>
+            <TouchableOpacity onPress={() => removeBlogPost(item.title)}>
+              <MaterialIcons name="delete" size={25} />
+            </TouchableOpacity>
+          </TouchableOpacity>
         )}
       />
     </>
@@ -56,6 +58,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginHorizontal: 20
+    marginHorizontal: 20,
+    borderColor: "grey",
+    borderBottomWidth: 1
   }
 });
