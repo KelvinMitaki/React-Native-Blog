b9066f146f92b646437e1b4c7e26379b064303ba
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-28 11:32:32 +0300

    added controlled inp to CreateScreen

--- src/screens/CreateScreen.tsx
+++ src/screens/CreateScreen.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
   return (
     <View>
-      <Text>CreateScreen CreateScreen</Text>
+      <Text style={styles.text}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setTitle(text)}
+        value={title}
+      />
+      <Text style={styles.text}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setContent(text)}
+        value={content}
+      />
     </View>
   );
 };
 
 export default CreateScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black"
+  }
+});
